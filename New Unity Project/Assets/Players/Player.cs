bc502973d556d27a13ea703643bfb165d888000b
Aashiq <aashiqalimohammed@gmail.com>
2017-03-17 16:36:24 +0530

    jsbdf dsfals

---
+++ New Unity Project/Assets/Players/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+private Vector3 inputvalue;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputvalue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+		inputvalue.y = 0f;
+		inputvalue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+		transform.Translate(inputvalue);
+	}
+}
